be50b3c4960b6df51646a90c6a63b56dd7a99a6a
Adalberto Façanha <afacanha66@gmail.com>
2023-05-06 23:01:15 -0300

    Nvim usado no notebook

---
+++ lua/keymaps.lua
@@ -0,0 +1,99 @@
+-- Shorten function name
+local keymap = vim.keymap.set
+-- Silent keymap option
+local opts = { silent = true }
+
+--Remap space as leader key
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+
+-- Modes
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
+
+-- Normal --
+-- Better window navigation
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+
+-- Resize with arrows
+keymap("n", "<C-Up>", ":resize 2<CR>", opts)
+keymap("n", "<C-Down>", ":resize +6<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize 2<CR>", opts)
+
+-- Navigate buffers
+keymap("n", "<C-Right>", ":vertical resize +6<CR>", opts)
+keymap("n", "<S-l>", ":bnext<CR>", opts)
+keymap("n", "<S-h>", ":bprevious<CR>", opts)
+
+-- Clear highlights
+keymap("n", "<leader>h", "<cmd>nohlsearch<CR>", opts)
+
+-- Close buffers
+keymap("n", "<S-q>", "<cmd>Bdelete!<CR>", opts)
+
+-- Better paste
+keymap("v", "p", '"_dP', opts)
+
+-- Insert --
+-- Press jj fast to enter
+keymap("i", "jj", "<ESC>", opts)
+
+-- Visual --
+-- Stay in indent mode
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
+-- Plugins --
+keymap("n", "<leader>a", ":Alpha<CR>", opts)
+
+-- NvimTree
+keymap("n", "<leader>f", ":NvimTreeToggle<CR>", opts)
+
+-- Telescope
+keymap("n", "<leader>ff", ":Telescope find_files<CR>", opts)
+keymap("n", "<leader>ft", ":Telescope live_grep<CR>", opts)
+keymap("n", "<leader>fp", ":Telescope projects<CR>", opts)
+keymap("n", "<leader>fb", ":Telescope buffers<CR>", opts)
+
+-- Git
+keymap("n", "<leader>gg", "<cmd>lua _LAZYGIT_TOGGLE()<CR>", opts)
+
+-- Comment
+keymap("n", "<leader>/", "<cmd>lua require('Comment.api').toggle.linewise.current()<CR>", opts)
+keymap("x", "<leader>/", '<ESC><CMD>lua require("Comment.api").toggle.linewise(vim.fn.visualmode())<CR>')
+
+-- Custom
+keymap("n", "<leader>p", "<cmd> PasteImg <CR>", opts)
+keymap("n", "<leader>e", "$", opts)
+keymap("n", "S", "<cmd> %s//g", opts)
+keymap("n", "<F8>", "<cmd> UndotreeToggle <CR> <cmd> UndotreeFocus <CR>", opts)
+keymap("n", "<C-\\>", "<cmd> TZAtaraxis <CR>", opts)
+keymap("n", "<Leader>4", "1gt<CR>", opts)
+keymap("n", "<Leader>5", "2gt<CR>", opts)
+keymap("n", "<Leader>6", "3gt<CR>", opts)
+keymap("n", "<Leader>7", "4gt<CR>", opts)
+keymap("n", "<Leader>8", "5gt<CR>", opts)
+keymap("n", "<Leader>t", "<cmd> tabnew<CR>", opts)
+keymap("n", "<Leader>c", "<cmd> tabclose<CR>", opts)
+-- Selecti all
+keymap('n', '<C-a>', 'gg<S-v>G')
+
+-- Move line up and down in NORMAL and VISUAL modes
+-- Reference: https://vim.fandom.com/wiki/Moving_lines_up_or_down
+keymap("n", "<M-Down>", "<CMD>move .+1<CR>")
+keymap("n", "<M-Up>", "<CMD>move .-2<CR>")
+keymap("x", "<M-Down>", ":move '>+1<CR>gv=gv")
+keymap("x", "<M-Up>", ":move '<-2<CR>gv=gv")
+-- Move line up and down in NORMAL and VISUAL modes
+-- Reference: https://vim.fandom.com/wiki/Moving_lines_up_or_down
+--keymap("n", "<C-j>", "<CMD>move .'>+4<CR>")
+--keymap("n", "<C-k>", "<CMD>move .1<CR>")
+--keymap("x", "<C-j>", ":move '>+4<CR>gv=gv")
+--keymap("x", "<C-k>", ":move '<1<CR>gv=gv"
